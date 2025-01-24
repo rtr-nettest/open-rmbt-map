e2dceaf639f88899fff63c1df5356942216820ce
Thomas Schreiber <thomas.schreiber@gmx.at>
2025-01-24 16:26:44 +0100

    support for iOS function calls

--- src/main/java/at/rtr/rmbt/map/constant/URIConstants.java
+++ src/main/java/at/rtr/rmbt/map/constant/URIConstants.java
@@ -4,7 +4,8 @@ public interface URIConstants {
     String VERSION = "/version";
 
     //https://m-cloud.netztest.at/RMBTMapServer/tiles/points/12/2232/1428.png?null&statistical_method=0.5&period=180&map_options=mobile/download
-    String TILES_ENDPOINT = "/tiles/{type}/{zoom}/{x}/{y}.png";
+    String TILES_ENDPOINT_VARS = "/tiles/{type}/{zoom}/{x}/{y}.png";
+    String TILES_ENDPOINT = "/tiles/{type}";
     public static enum TILE_TYPE {points, shapes, heatmap}
 
     String MARKERS = "/tiles/markers";
--- src/main/java/at/rtr/rmbt/map/controller/TilesController.java
+++ src/main/java/at/rtr/rmbt/map/controller/TilesController.java
@@ -10,9 +10,13 @@ import at.rtr.rmbt.map.util.TileParameters;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,7 +29,7 @@ public class TilesController {
 
     ////https://m-cloud.netztest.at/RMBTMapServer/tiles/points/12/2232/1428.png?null&statistical_method=0.5&period=180&map_options=mobile/download
     @Operation(summary = "Get point tile for a specific point /zoom/x/y")
-    @GetMapping(value = URIConstants.TILES_ENDPOINT, produces = "image/png")
+    @GetMapping(value = URIConstants.TILES_ENDPOINT_VARS, produces = "image/png")
     //Unfortunately, DTO with renaming params seems to not be supported, therefore this list
     public byte[] getTiles(@PathVariable URIConstants.TILE_TYPE type,
                            @PathVariable int zoom,
@@ -65,6 +69,44 @@ public class TilesController {
             }
         }
         return null;
+    }
+
+    @Operation(summary = "Get point tile for a specific point /zoom/x/y")
+    @GetMapping(value = URIConstants.TILES_ENDPOINT, produces = "image/png")
+    //Unfortunately, DTO with renaming params seems to not be supported, therefore this list
+    public ResponseEntity<byte[]> getTilesbyPath(@PathVariable URIConstants.TILE_TYPE type,
+                           @RequestParam(name = "path", required = true) String path,
+                           @RequestParam(name = "statistical_method", required = false) Float statisticalMethod,
+                           @RequestParam(name = "size", required = false) Double size,
+                           @RequestParam(name = "map_options") String mapOptions,
+                           @RequestParam(name = "developerCode", required = false) String developerCode,
+                           @RequestParam(name = "transparency", required = false) Double transparency,
+                           @RequestParam(name = "point_diameter", required = false) Double pointDiameter,
+                           @RequestParam(name = "no_fill", required = false) Boolean noFill,
+                           @RequestParam(name = "no_color", required = false) Boolean noColor,
+                           @RequestParam(name = "hightlight", required = false) UUID hightlightUuid,
+                           @RequestParam(name = "operator", required = false) String operator,
+                           @RequestParam(name = "provider", required = false) String provider,
+                           @RequestParam(name = "technology", required = false) String technology,
+                           @RequestParam(name = "period", required = false) Integer period,
+                           @RequestParam(name = "age", required = false) Integer age,
+                           @RequestParam(name = "user_server_selection", required = false) String userServerSelection) {
+        //used by iOS
+        //3/5/6
+        List<Integer> pathParts;
+        try {
+            pathParts = Arrays.stream(path.split("/")).map(i -> Integer.parseInt(i)).collect(Collectors.toList());
+            if (pathParts.size() != 3) {
+                throw new RuntimeException("Invalid path parameters");
+            }
+        }
+        catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(null);
+        }
+
+        return ResponseEntity.ok(getTiles(type, pathParts.get(0), pathParts.get(1), pathParts.get(2),
+                statisticalMethod, size.intValue(), mapOptions, developerCode, transparency, pointDiameter,
+                noFill, noColor, hightlightUuid, operator, provider, technology, period, age, userServerSelection));
     }
 
 
