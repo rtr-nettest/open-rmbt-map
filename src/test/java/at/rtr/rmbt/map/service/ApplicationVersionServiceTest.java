53155b81addb511dc4c1efaa8091de2da3fae19b
Thomas Schreiber <thomas.schreiber@gmx.at>
2025-09-01 09:09:39 +0200

    mock application version test

--- src/test/java/at/rtr/rmbt/map/service/ApplicationVersionServiceTest.java
+++ src/test/java/at/rtr/rmbt/map/service/ApplicationVersionServiceTest.java
@@ -2,15 +2,22 @@ package at.rtr.rmbt.map.service;
 
 import at.rtr.rmbt.map.MapServerConfiguration;
 import at.rtr.rmbt.map.dto.ApplicationVersionResponse;
+import at.rtr.rmbt.map.model.Settings;
+import at.rtr.rmbt.map.repository.SettingsRepository;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @ExtendWith(SpringExtension.class)
@@ -20,13 +27,16 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ActiveProfiles("test")
 class ApplicationVersionServiceTest {
 
-    @Autowired
     private ApplicationVersionService applicationVersionService;
+
+    @Mock
+    private SettingsRepository settingsRepository;
 
 
     @BeforeEach
     public void setUp() {
-        //applicationVersionService = new ApplicationVersionService(settingsRepository);
+        Mockito.when(settingsRepository.findFirstByKey(ApplicationVersionService.SYSTEM_UUID_KEY)).thenReturn(Optional.of(new Settings(1L,ApplicationVersionService.SYSTEM_UUID_KEY,null,"abcde")));
+        applicationVersionService = new ApplicationVersionService(settingsRepository);
 
     }
 
@@ -35,6 +45,7 @@ class ApplicationVersionServiceTest {
     @Test
     void testGetApplicationVersion() {
         ApplicationVersionResponse applicationVersion = applicationVersionService.getApplicationVersion();
-        System.out.println(applicationVersion);
+        assertEquals("abcde",applicationVersion.getSystemUUID());
+
     }
 }
